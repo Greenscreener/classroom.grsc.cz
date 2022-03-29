92a2d3cf7b0de7e1e34b46fbb9681efca82eb0dd
Greenscreener <honzikcernoh@gmail.com>
2022-03-29 16:51:33 +0200

    The app now remembers all non-recent assignments from 'Long Refresh' and automatically checks them every time.

--- public/scripts/fetch-assignments.js
+++ public/scripts/fetch-assignments.js
@@ -7,6 +7,45 @@ const defaultDueTimeLimit = 2*7*24*60*60*1000; // 2 weeks
 // How far (milliseconds) into the past are we willing to search for unfinished assignments without a due time by update time. (I hate this)
 const defaultUpdateTimeLimit = undefined; // default is 2*dueTimeLimit
 
+function pin(course, assignment) {
+	course = course.toString()
+	assignment = assignment.toString()
+	let pins = JSON.parse(window.localStorage.getItem("pins"))
+	if (pins === null) {
+		pins = {}
+	}
+	if (!(course in pins)) {
+		pins[course] = []
+	}
+	if (pins[course].indexOf(assignment) === -1) {
+		pins[course].push(assignment)
+	}
+	window.localStorage.setItem("pins", JSON.stringify(pins))
+}
+
+function unpin(course, assignment) {
+	course = course.toString()
+	assignment = assignment.toString()
+	const pins = JSON.parse(window.localStorage.getItem("pins"))
+	if (pins === null) {
+		return
+	}
+	if (course in pins) {
+		if (pins[course].indexOf(assignment) !== -1) {
+			pins[course] = pins[course].filter(e => e !== assignment)
+			window.localStorage.setItem("pins", JSON.stringify(pins))
+		}
+	}
+}
+function getPins(course) {
+	if (JSON.parse(window.localStorage.getItem("pins")) === null) {
+		return []
+	} 
+	if (!(course in JSON.parse(window.localStorage.getItem("pins")))) {
+		return []
+	}
+	return JSON.parse(window.localStorage.getItem("pins"))[course]
+}
 
 function fetchAssignments(dueTimeLimit = defaultDueTimeLimit, updateTimeLimit = defaultUpdateTimeLimit) {
 	return new Promise((resolve, reject) => {
@@ -32,25 +71,49 @@ function fetchAssignments(dueTimeLimit = defaultDueTimeLimit, updateTimeLimit = 
 						// Determine if the assignment is recent enough to make a request about
 						return isRecent(e, dueTimeLimit, updateTimeLimit);
 					});
-					async.eachLimit(recentAssignments, requestLimit, (assignment,callback) => {
-						debugRequests++;
-						fetch(`https://classroom.googleapis.com/v1/courses/${course.id}/courseWork/${assignment.id}/studentSubmissions?pageSize=1&userId=me`,{
+					pinIds = getPins(course.id.toString())
+					async.eachLimit(pinIds, requestLimit, (assignmentId, callback) => {
+						fetch(`https://classroom.googleapis.com/v1/courses/${course.id}/courseWork/${assignmentId}`,{
 							headers: {
 								Authorization: "Bearer " + Cookies.get("googleAuthToken")
 							}
 						}).then(response => {
-							if (response.status === 404 && assignment.assigneeMode !== "ALL_STUDENTS") {
-								callback();
+							if (response.status === 404) {
+								unpin(course.id, assignmentId)
+								callback()
 							} else {
 								response.json().then(json => {
-									if (["TURNED_IN","RETURNED"].indexOf(json.studentSubmissions[0].state) === -1) {
-										dueAssignments.push({assignment: assignment, submission: json.studentSubmissions[0], defaultRecent: isRecent(assignment, defaultDueTimeLimit, defaultUpdateTimeLimit)});
-									}
-									callback();
-								});
+									recentAssignments.push(json)
+									callback()
+								})
 							}
 						})
-					}).then(() => callback())
+					}).then(() => {
+						async.eachLimit(recentAssignments, requestLimit, (assignment,callback) => {
+							debugRequests++;
+							fetch(`https://classroom.googleapis.com/v1/courses/${course.id}/courseWork/${assignment.id}/studentSubmissions?pageSize=1&userId=me`,{
+								headers: {
+									Authorization: "Bearer " + Cookies.get("googleAuthToken")
+								}
+							}).then(response => {
+								if (response.status === 404 && assignment.assigneeMode !== "ALL_STUDENTS") {
+									callback();
+								} else {
+									response.json().then(json => {
+										if (["TURNED_IN","RETURNED"].indexOf(json.studentSubmissions[0].state) === -1) {
+											dueAssignments.push({assignment: assignment, submission: json.studentSubmissions[0], defaultRecent: isRecent(assignment, defaultDueTimeLimit, defaultUpdateTimeLimit)});
+											if (!isRecent(assignment, defaultDueTimeLimit, defaultUpdateTimeLimit)) {
+												pin(course.id.toString(), assignment.id.toString())
+											}
+										} else {
+											unpin(course.id.toString(), assignment.id.toString())
+										}
+										callback();
+									});
+								}
+							})
+						}).then(() => callback())
+					})
 				});
 			}).then(() => { console.log("Number of requests made: ", debugRequests); resolve() });
 		});
@@ -75,6 +138,5 @@ function zeropad(strings, ...inputs) {
 }
 
 function isRecent(e, dueTimeLimit, updateTimeLimit= dueTimeLimit*2) {
-	console.log(updateTimeLimit);
 	return (typeof e.dueDate === "undefined" && new Date(e.updateTime) > new Date(Date.now()-updateTimeLimit)) || dueDateToDate(e.dueDate, e.dueTime) > new Date(Date.now()-dueTimeLimit);
 }
